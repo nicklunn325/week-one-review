6b2e0e9e98943f276629369b215640bf8a43e1f3
flatlandnick <nicholas.lunn@flatironschool.com>
2022-02-28 07:53:25 -0600

    initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# WEEK ONE REVIEW
+
+## TOPICS
+
+- data types
+- variable declaration
+- if statements
+- comparison operators
+- functions
+  - declaring vs invoking
+  - arrow functions
+  - first class functions
+  - callback functions
+- arrays
+  - accessing
+  - modifying
+  - iterating
+    - forEach
+    - map
+    - filter
+    - find
+  - pass by value vs pass by reference
+- objects
+  - accessing
+  - modifying
---
+++ index.js
@@ -0,0 +1,31 @@
+// data types
+
+const dataTypes = [
+  true,
+  null,
+  undefined,
+  "string",
+  5,
+  Symbol("symbol"),
+  BigInt(23),
+];
+
+dataTypes.forEach((element) => {
+  console.log(element, typeof element);
+});
+
+// let vs const
+
+// comparison && logical operators
+
+// if statements
+
+// functions
+
+// arrays
+
+const numbers = [1, 2, 3, 4, 5];
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+// objects
